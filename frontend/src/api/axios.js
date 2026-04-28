77fb6e1ad0db0b91a19b4e46e3aba6746b4a4ba3
Kaviyak0105 <kaviyakittu246@gmail.com>
2026-04-28 08:59:47 +0530

    modified code

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+baseURL: 'https://online-exam-system-yb28.onrender.com/api'
 });
 
 // Every request la automatically token add pannum
